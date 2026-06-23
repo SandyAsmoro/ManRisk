8d2badf0fe08b9216bbc25c7da34d4b32a378895
sandy <sandyasmoro13@gmail.com>
2026-06-24 04:55:26 +0700

    FIX : api.js

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -3,7 +3,8 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || 'https://man-risk-mski-y53k-4ehnugklk-kala-s-projects1.vercel.app/api/',
+  // baseURL: import.meta.env.VITE_API_URL || 'https://man-risk-mski-y53k-4ehnugklk-kala-s-projects1.vercel.app/api/',
+  baseURL: import.meta.env.VITE_API_URL || 'https://man-risk.vercel.app/api',
   headers: {
     'Content-Type': 'application/json'
   }
